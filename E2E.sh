3114799d84d1313d11bab15008e5a28ffb4faa2b
eyal <eyalbran6b@gmail.com>
2023-03-02 16:09:14 +0200

    tag6

--- E2E.sh
+++ E2E.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-for i in {1..20}; do home=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:5000); if [ $home -eq 200 ]; then break; fi; echo "Curling localhost:5000 Faild: $i/20" && sleep 1; done
+for i in {1..20}; do home=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80); if [ $home -eq 200 ]; then break; fi; echo "Curling localhost:80 Faild: $i/20" && sleep 1; done
 # Register a new user
-response=$(curl -s -X POST -d "firstname=John&lastname=Doe&birthdate=1990-01-01&city=New York&username=johndoe&password=pass123" http://localhost:5000/register)
+response=$(curl -s -X POST -d "firstname=John&lastname=Doe&birthdate=1990-01-01&city=New York&username=johndoe&password=pass123" http://localhost:80/register)
 if [ "$response" != "Thank you for registering!" ]; then
   echo "Error: Registration failed"
   exit 1
@@ -11,14 +11,14 @@ fi
 
 # Register a new event
 
-response=$(curl -s -X POST -d "name=Event1" http://localhost:5000/vRegister)
+response=$(curl -s -X POST -d "name=Event1" http://localhost:80/vRegister)
 if [ "$response" != "Event1" ]; then
   echo "Error: Event registration failed"
   exit 1
 fi
 
 # Check the user's events
-response=$(curl -s http://localhost:5000/myevents)
+response=$(curl -s http://localhost:80/myevents)
 if ! echo "$response" | grep -q "שם האירוע"; then
   echo "Error: User's events not found"
   exit 1
